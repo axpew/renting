7c106d491d30cc6b767e064ac3a2a257dc71183a
Alejandro Porras <ale.porras.oroz.0807@gmail.com>
2026-03-19 10:55:09 -0600

    com1

--- src/main/java/com/videocartago/renting/controller/WelcomeController.java
+++ src/main/java/com/videocartago/renting/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.videocartago.renting.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
@@ -7,7 +8,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WelcomeController {
    @RequestMapping(value="/", method=RequestMethod.GET)
-   public String welcome(){
+   public String welcome(Model model){
+        model.addAttribute("miNombre", "Alejandro");
        return "welcome"; // corresponde al nombre de la plantilla
    }
 }
